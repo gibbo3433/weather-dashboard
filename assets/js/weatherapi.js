cde6b381809bb054b0f620adc6f50132bb3082c0
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2022-10-31 21:36:21 +0000

    added event listener for search buttton

---
+++ assets/js/weatherapi.js
@@ -0,0 +1,16 @@
+var searchbutton = document.getElementById('search-button');
+var searchtext = document.getElementById('search-text')
+
+
+
+function searchweather() {
+
+console.log(searchtext.value)
+
+}
+
+
+
+searchbutton.addEventListener("click", searchweather);
+
+
