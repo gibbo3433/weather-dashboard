509d984127f45cb0324c905bfdb023e1e229ee0e
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2022-11-05 15:27:57 +0000

    findchosloc working fully

--- assets/js/weatherapi.js
+++ assets/js/weatherapi.js
@@ -1,6 +1,9 @@
-var searchbutton = document.getElementById('search-button');
-var searchtext = document.getElementById('search-text')
 
+var searchbutton = document.getElementById('search-button');
+var searchtext = document.getElementById('search-text');
+
+// chosenlocation becomes what the user entered into the search text input
+var chosenlocation = searchtext.value;
 
 // This fucntion adds the users choice of location to the console.log
 function searchweatherlocation() {
@@ -11,20 +14,35 @@ console.log(searchtext.value)
 // If the user enters no text into the input, a windows error will be displayed
 if (chosenlocation === '') {
     window.alert('Error, please enter a valid location')
+    return;
 // If the user enters a valid location, then the findchosenlocation function will begin
 } else {
+    console.log(chosenlocation)
     findchosenlocation(chosenlocation)
 }}
 
-// Finds the chosen location ......
-function findchosenlocation
 
+  
+// Finds the chosen location from the API
+function findchosenlocation (input) {
 
+    // This will add my users chosenlocation in to the API search and add my API key so that it allows me the chosen data
+    var findchosenweather = `https://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=5&appid=d91f911bcf2c0f925fb6535547a5ddc9`;
 
+    // fetch the data from the api, related to what the user inputed
+    fetch (findchosenweather)
 
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        // Show the data found in the console
+        console.log(data);
+      });
+    }
+
+ 
 
 
 
 searchbutton.addEventListener("click", searchweatherlocation);
-
-
