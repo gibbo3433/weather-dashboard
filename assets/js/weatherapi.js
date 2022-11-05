191a139c3610f541f7c872a2347e85bb2c349cba
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2022-11-05 13:00:58 +0000

    added search location funct + notes

--- assets/js/weatherapi.js
+++ assets/js/weatherapi.js
@@ -2,15 +2,29 @@ var searchbutton = document.getElementById('search-button');
 var searchtext = document.getElementById('search-text')
 
 
+// This fucntion adds the users choice of location to the console.log
+function searchweatherlocation() {
 
-function searchweather() {
-
+var chosenlocation = searchtext.value;
 console.log(searchtext.value)
 
-}
+// If the user enters no text into the input, a windows error will be displayed
+if (chosenlocation === '') {
+    window.alert('Error, please enter a valid location')
+// If the user enters a valid location, then the findchosenlocation function will begin
+} else {
+    findchosenlocation(chosenlocation)
+}}
+
+// Finds the chosen location ......
+function findchosenlocation
 
 
 
-searchbutton.addEventListener("click", searchweather);
+
+
+
+
+searchbutton.addEventListener("click", searchweatherlocation);
 
 
